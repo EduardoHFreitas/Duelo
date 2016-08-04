bb4f2aac1dcaa5c9c1916450d650d954cf407c41
Eduardo Henrique de Freitas <eduardo.freitas@datacoper.com.br>
2016-08-03 22:31:36 -0300

    pistoleiros abstratos

--- src/br/univel/duelo/pistoleiro/AbstractPistoleiro.java
+++ src/br/univel/duelo/pistoleiro/AbstractPistoleiro.java
@@ -1,9 +1,6 @@
 package br.univel.duelo.pistoleiro;
 
-import java.util.Random;
-
 import br.univel.duelo.arma.Arma;
-import br.univel.duelo.pistoleiro.usa.Pistoleiro;
 
 /**
  * @author Eduardo
@@ -14,10 +11,13 @@ public abstract class AbstractPistoleiro implements Pistoleiro{
 	private final String nome;
 	private Float vida;
 	private Arma arma;
+	private final Float precisao;
 
-	protected AbstractPistoleiro(final String nome, final Float vida) {
+	protected AbstractPistoleiro(final String nome, final Float vida, final Float precisao) {
 		this.nome = nome;
 		this.vida = vida;
+		this.precisao = precisao;
+
 	}
 
 	@Override
@@ -26,57 +26,28 @@ public abstract class AbstractPistoleiro implements Pistoleiro{
 		return this;
 	}
 
-	protected Pistoleiro (String nome) {
-		this.nome = nome;
+	@Override
+	public Float atirar(){
+		if (this.arma != null){
+			return this.arma.atirar() * precisao;
+		}
+		System.out.println(String.format("Pistoleiro %s nao tem arma!", getNome()));
+		return 0f;
 	}
 
-	public String getNome() {
-		return nome;
-	}
-
-	/**
-	 * Faz o disparo do pistoleiro, o disparo � baseado na for�a que um tiro
-	 * pode ter, sendo de 0 .. 100
-	 *
-	 * @return for�a que o tira tem
-	 */
-	public Float atirar() {
-		// Para atirar utiliza um Random para gerar a for�a do disparo, por�m
-		// quando se utilizar nextFloat � gerado um n�mero entre 0.0 a 1.0, por
-		// isso � necess�rio multiplicar por 100 para termos um pot�ncia maior
-		// no tiro. Exemplo, se o random gerar 0.9 ap�s a multiplica��o teremos
-		// um tiro com 90 de for�a
-		return new Random().nextFloat() * 70;
-	}
-
-	/**
-	 * Utilizado pelo pistoleiro para defender um tiro disparado pelo outro, a
-	 * for�a do tiro � subtraida da vida do pistoleiro
-	 *
-	 * @param tiro
-	 */
-	public Pistoleiro defenderTiro(Float tiro) {
-		// O codigo abaixo � o mesmo que vida = vida - tiro;
-		Float perda = vida;
-		vida -= tiro;
-		System.out.println(this.getNome() + " perdeu " + (perda - vida) + " de vida");
+	@Override
+	public Pistoleiro defenderTiro(Float tiro){
+		this.vida -= tiro;
 		return this;
 	}
 
-	/**
-	 * Informa se o pistoleiro esta vivo
-	 *
-	 * @return true para vivo e false para morto
-	 */
-	public Boolean estaVivo() {
-		// C�digo vida > 0 � igual ao c�digo abaixo, por�m de forma mais
-		// reduzida, � retornado direto um boolean para vida maior que zero, o
-		// retorno � boolean devido a defini��o no m�todo
-		// if (vida > 0) {
-		// return true;
-		// } else {
-		// return false;
-		// }
-		return vida > 0;
+	@Override
+	public Boolean estaVivo(){
+		return this.vida > 0;
+	}
+
+	@Override
+	public String getNome(){
+		return this.nome;
 	}
 }
--- src/br/univel/duelo/pistoleiro/br/Cangaceiro.java
+++ src/br/univel/duelo/pistoleiro/br/Cangaceiro.java
@@ -2,82 +2,12 @@ package br.univel.duelo.pistoleiro.br;
 
 import java.util.Random;
 
+import br.univel.duelo.pistoleiro.AbstractPistoleiro;
 import br.univel.duelo.pistoleiro.Pistoleiro;
 
-/**
- * Representa um pistoleiro no nosso joguinho com as a��es b�sicas deste, sendo
- * atirar e defender tiro
- *
- * @author Will
- *
- */
-class Cangaceiro implements Pistoleiro {
+class Cangaceiro extends AbstractPistoleiro {
 
-	/**
-	 * Armazena em tempo de execu��o o nome do pistoleiro
-	 */
-	private String nome;
-	/**
-	 * Armazena em tempo de execu��o o quantidade de vida que o pistoleiro ainda
-	 * tem, todo pistoleiro come�a com a vida em 100%
-	 */
-	private float vida = 80;
-
-	/**
-	 * Construtor para cria��o de um pistoleiro novo, sendo necess�rio informar
-	 * um nome para o mesmo
-	 *
-	 * @param nome
-	 */
 	protected Cangaceiro(String nome) {
-		this.nome = nome;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	/**
-	 * Faz o disparo do pistoleiro, o disparo � baseado na for�a que um tiro
-	 * pode ter, sendo de 0 .. 100
-	 *
-	 * @return for�a que o tira tem
-	 */
-	public Float atirar() {
-		// Para atirar utiliza um Random para gerar a for�a do disparo, por�m
-		// quando se utilizar nextFloat � gerado um n�mero entre 0.0 a 1.0, por
-		// isso � necess�rio multiplicar por 100 para termos um pot�ncia maior
-		// no tiro. Exemplo, se o random gerar 0.9 ap�s a multiplica��o teremos
-		// um tiro com 90 de for�a
-		return new Random().nextFloat() * 70;
-	}
-
-	/**
-	 * Utilizado pelo pistoleiro para defender um tiro disparado pelo outro, a
-	 * for�a do tiro � subtraida da vida do pistoleiro
-	 *
-	 * @param tiro
-	 */
-	public Cangaceiro defenderTiro(Float tiro) {
-		// O codigo abaixo � o mesmo que vida = vida - tiro;
-		vida -= tiro;
-		return this;
-	}
-
-	/**
-	 * Informa se o pistoleiro esta vivo
-	 *
-	 * @return true para vivo e false para morto
-	 */
-	public Boolean estaVivo() {
-		// C�digo vida > 0 � igual ao c�digo abaixo, por�m de forma mais
-		// reduzida, � retornado direto um boolean para vida maior que zero, o
-		// retorno � boolean devido a defini��o no m�todo
-		// if (vida > 0) {
-		// return true;
-		// } else {
-		// return false;
-		// }
-		return vida > 0;
+		super(nome, 100f, 7f);
 	}
 }
